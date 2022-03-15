23988e5e4315cd76d66439057562658f28da4ece
DraganSRB <dragan94@gmail.com>
2022-03-15 14:21:46 +0100

    Main Page, App icon

--- EkipaNaKvadratCookBook/EkipaNaKvadratCookBook/MainPage.xaml.cs
+++ EkipaNaKvadratCookBook/EkipaNaKvadratCookBook/MainPage.xaml.cs
@@ -14,5 +14,10 @@ namespace EkipaNaKvadratCookBook
         {
             InitializeComponent();
         }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 }
